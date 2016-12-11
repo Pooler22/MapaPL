60ffc25b4b1843298ef4a990837efca6ff31030e
pooler22 <rudypawel12@gmail.com>
2016-12-11 13:08:04 +0100

    add autorezise map

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let map;
 let marker;
+let isOpenPanel = true;
 let edited = false;
 const campus = [
     {
@@ -38,6 +39,15 @@ function updateMarker(latIn, lngIn) {
         map: map
     });
     map.setCenter(marker.getPosition());
+    if (window.innerWidth < 760) {
+        let sidedrawerEl = document.getElementById('sidedrawer');
+        sidedrawerEl.className = sidedrawerEl.className.replace(' active', '');
+        document.body.appendChild(sidedrawerEl);
+        mui.overlay('off');
+//        mui.overlay('on', options).appendChild(sidedrawerEl);
+//        setTimeout(() => sidedrawerEl.className += ' active', 20);
+
+    }
 }
 
 function initMap(latIn = 51.752845, lngIn = 19.453180, zoomIn = 18) {
@@ -108,7 +118,7 @@ function filterList(value) {
 function showSidedrawer() {
     let sidedrawerEl = document.getElementById('sidedrawer');
     let options = {
-        onclose: function () {
+        onclose: () => {
             sidedrawerEl.className = sidedrawerEl.className.replace(' active', '');
             document.body.appendChild(sidedrawerEl);
         }
@@ -121,8 +131,12 @@ function showSidedrawer() {
 function toggleSidedrawer() {
     let body = document.getElementsByTagName('BODY')[0];
     if (body.className == 'hide-sidedrawer') {
-        body.className = 'show-sidedrawer'
+        body.className = 'show-sidedrawer';
+        isOpenPanel = true;
+        updateMapSize()
     } else {
+        isOpenPanel = false;
+        updateMapSize();
         body.className = 'hide-sidedrawer'
     }
 }
@@ -138,7 +152,39 @@ function toggleListElement(element) {
     }
 }
 
+function updateMapSize() {
+    if (window.innerWidth < 760) {
+        isOpenPanel = false;
+    }
+    document.getElementById('map').style.height = ((window.innerHeight - 64) + 'px');
+    if (isOpenPanel) {
+        document.getElementById('map').style.width = ((window.innerWidth - 200) + 'px');
+        document.getElementById('map').style.margin = '0 0 0 200px';
+        // document.getElementById('map').setAttribute('style','width:'+ (window.innerWidth - 200)+'px');
+    }
+    else {
+        document.getElementById('map').style.width = ((window.innerWidth) + 'px');
+        document.getElementById('map').style.margin = '0';
+
+        // document.getElementById('map').setAttribute('style','width:'+ window.innerWidth+'px');
+    }
+
+}
+
+window.onresize = (event) => {
+    if (window.innerWidth < 760) {
+        isOpenPanel = false;
+    }
+    else {
+        isOpenPanel = true;
+    }
+    mui.overlay('off');
+    updateMapSize();
+};
+
+
 function init() {
+    updateMapSize();
     initList();
     document.getElementsByClassName('js-show-sidedrawer')[0].addEventListener('click', showSidedrawer, false);
     document.getElementsByClassName('js-hide-sidedrawer')[0].addEventListener('click', toggleSidedrawer, false);
