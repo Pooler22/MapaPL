0a50f18d8bd6d55fd22985853735fa1f30ae79bc
pawelrutkowski <rutkowskip22@gmail.com>
2018-03-11 01:34:33 +0100

    refactor

--- src/Containers/MainPage/AppBar.js
+++ src/Containers/MainPage/AppBar.js
@@ -44,30 +44,6 @@ const styles = theme => ({
   },
   hide: {
     display: 'none'
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  'content-right': {
-    marginRight: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  'contentShift-left': {
-    marginLeft: 0
-  },
-  'contentShift-right': {
-    marginRight: 0
   }
 });
 
