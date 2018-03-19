b7ca024d80cb46a6284120d117a42d9752a5a98a
pawelrutkowski <rutkowskip22@gmail.com>
2018-03-19 17:20:17 +0100

    update indexhtml

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,17 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import initStoryshots from '@storybook/addon-storyshots';
 
+configure({ adapter: new Adapter() });
+
+// Mock matchMedia
+global.window.matchMedia = jest.fn(() => ({
+  matches: false,
+  addListener: jest.fn(),
+  removeListener: jest.fn()
+}));
+
+jest.mock('scroll-to-element', () => 'scroll-to-element');
+
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
@@ -10,5 +21,3 @@ const localStorageMock = {
 global.localStorage = localStorageMock;
 
 initStoryshots();
-
-configure({ adapter: new Adapter() });
