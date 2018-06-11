c9b34d0180b721cbdab02058fa306945935a3381
Pawel Rutkowski <rutkowskip22@gmail.com>
2018-06-11 22:13:35 +0200

    configure sentry

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,24 +1,28 @@
-import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
-import Reboot from "material-ui/Reboot";
-
-import MainPage from "../MainPage";
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Reboot from 'material-ui/Reboot';
+import Raven from 'raven-js';
+import MainPage from '../MainPage';
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: "#730007",
-      main: "#730007",
-      dark: "#730007"
+      light: '#730007',
+      main: '#730007',
+      dark: '#730007',
     },
     secondary: {
-      light: "#730007",
-      main: "#730007",
-      dark: "#730007"
-    }
-  }
+      light: '#730007',
+      main: '#730007',
+      dark: '#730007',
+    },
+  },
 });
+
+Raven.config(
+  'https://c185eda6089740c5904068dfc02ccbe8@sentry.io/1223712'
+).install();
 
 class App extends React.Component {
   render() {
